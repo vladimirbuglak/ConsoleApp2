f7bd02b540ac25fcf87221563d40e08ed7b1f49e
vladimir <u.buhlak@gmail.com>
2019-03-03 23:16:36 +0300

    change

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,7 +7,6 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
 
         public static void F()
