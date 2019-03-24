602a6f3674f6b189ead738b6ba89b6eabd478330
Vladimir <u.buhlak@gmail.com>
2019-03-24 22:42:02 +0300

    test2 in master

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -26,6 +26,7 @@ namespace ConsoleApp2
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
